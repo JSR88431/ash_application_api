89428130bdc5f2406ca07320693c385c4dd755a6
jsr88431 <jsr88431@gmail.com>
2025-04-25 11:23:10 -0700

    add url and payload

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+
+url = 'https://apply-to-avantos.dev-sandbox.workload.avantos-ai.net'
+data = {
+    'email': 'josephsonreynoso@gmail.com'
+    }
+
+response = requests.post(url, json=data)
+
+print('Status Code:', response.status_code)
+print('Response Body:', response.json())
