4c4089c91f5e895bb9e27d1d1add24a335503647
jsr88431 <jsr88431@gmail.com>
2025-04-25 11:32:27 -0700

    add headers with user agent

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ data = {
     'email': 'josephsonreynoso@gmail.com'
     }
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/133.0.0.0 Safari/537.36'
+}
+
 response = requests.post(url, json=data)
 
 print('Status Code:', response.status_code)
